8f5396521cc20ca4a8e2cc08b27bcf946e6c9af3
geekcomputers <craig@geekcomputers.co.uk>
2013-06-16 13:12:21 +0100

    Shows the object causing the waits

--- dbfile_seqread_object.sql
+++ dbfile_seqread_object.sql
@@ -1,10 +1,12 @@
-REM Filename      : dbfile_seqread_object.sql
-REM Author        : Craig Richards
-REM Created       : 06-August-2008
-REM Version       : 1.0
-REM Modifications :
+REM Script Name		: dbfile_seqread_object.sql
+REM Author				: Craig Richards
+REM Created			: 06-August-2008
+REM Last Modified	:
+REM Version				: 1.0
 REM
-REM Description   : Shows the object causing the waits 
+REM Modifications		:
+REM
+REM Description		: Shows the object causing the waits 
 
 SELECT   b.sid, nvl(substr(a.object_name,1,30),'P1='||b.p1||' P2='||b.p2||' P3='||b.p3) object_name,
          a.subobject_name,
